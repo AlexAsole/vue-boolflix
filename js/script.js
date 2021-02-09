575d61d522ba41a8f45f112c7f3a7d8338b5947b
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-09 15:24:30 +0100

    layout base

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ new Vue({
                 .get(`https://api.themoviedb.org/3/search/movie?api_key=beb26a5871cd03140d2c6b0bb0463b7e&query=${self.searchMovie}`)
                 .then(function(resp) {
                     self.movies = resp.data.results
+                    self.searchMovie = ''
                 })
         }
-
     }
 }) 
 Vue.config.devtools = true;
