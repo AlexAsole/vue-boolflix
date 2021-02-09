d638ef33a7c642953729445fa7619414bd9eb6d3
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-09 12:33:07 +0100

    ricerca fatta

---
+++ js/script.js
@@ -0,0 +1,19 @@
+new Vue({
+    el:'#root',
+    data: {
+        movies: [],
+        searchMovie: ''
+    },
+    methods: {
+        createMoviesList: function() {
+            const self = this;
+            axios
+                .get(`https://api.themoviedb.org/3/search/movie?api_key=beb26a5871cd03140d2c6b0bb0463b7e&query=${self.searchMovie}`)
+                .then(function(resp) {
+                    self.movies = resp.data.results
+                })
+        }
+
+    }
+}) 
+Vue.config.devtools = true;
