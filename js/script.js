7961b619102f7818edcdbf10cc5e1ce7ae42205a
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-10 10:57:25 +0100

    stelle mostrate

--- js/script.js
+++ js/script.js
@@ -9,9 +9,15 @@ new Vue({
             const self = this;
             axios
                 .get(`https://api.themoviedb.org/3/search/movie?api_key=beb26a5871cd03140d2c6b0bb0463b7e&query=${self.searchMovie}`)
-                .then(function(resp) {
-                    self.movies = resp.data.results
-                    self.searchMovie = ''
+                .then(function (resp) {
+                        self.movies = resp.data.results;
+                        self.voteTo5()
+                        self.searchMovie = '';
+                    })
+        },
+        voteTo5: function() {
+            this.movies.forEach((item) => {
+                item.vote_average = Math.round(item.vote_average / 2)
                 })
         }
     }
