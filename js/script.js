d594f3f4718a2c360140d6312aa25d861d358491
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-10 18:15:40 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -2,24 +2,40 @@ new Vue({
     el:'#root',
     data: {
         movies: [],
-        searchMovie: ''
+        series: [],
+        searchProduct: '',
+        flags: ['it','cn','de','en','es','fr','ja','ko','ru','pt','nl']
     },
     methods: {
-        createMoviesList: function() {
+        createProductsList: function() {
             const self = this;
             axios
-                .get(`https://api.themoviedb.org/3/search/movie?api_key=beb26a5871cd03140d2c6b0bb0463b7e&query=${self.searchMovie}`)
+                .get('https://api.themoviedb.org/3/search/movie' ,{
+                    params:{
+                        api_key : 'beb26a5871cd03140d2c6b0bb0463b7e',
+                        query : self.searchProduct
+                    }
+                })
                 .then(function (resp) {
                         self.movies = resp.data.results;
-                        self.voteTo5()
-                        self.searchMovie = '';
+                        self.searchProduct = '';
                     })
-        },
-        voteTo5: function() {
-            this.movies.forEach((item) => {
-                item.vote_average = Math.round(item.vote_average / 2)
+            axios
+                .get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: 'beb26a5871cd03140d2c6b0bb0463b7e',
+                        query: self.searchProduct
+                    }
                 })
+                .then(function (resp) {
+                    self.tvSeries = resp.data.results;
+                    self.searchProduct = '';
+                })
+        },
+        voteTo5: function(element) {
+            return Math.round(element/2)
         }
     }
 }) 
 Vue.config.devtools = true;
+
